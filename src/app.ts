087db3c568939e027081c67e21b1c249dd3bd885
gjlam95 <gj_lam@hotmail.com>
2022-06-13 10:23:27 +0800

    update eslint

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.get('/members', async (req, res) => {
 // Throws an error if member already exist
 app.post('/members', async (req, res) => {
   try {
-    const member = await Member.create(req.body);
+    await Member.create(req.body);
     return res.status(200).send('Member added');
   } catch (err) {
     return res.status(500).json(err);
@@ -57,7 +57,7 @@ app.get('/qualifications', async (req, res) => {
 // Throws an error if qualification already exist
 app.post('/qualifications', async (req, res) => {
   try {
-    const qualification = await Qualification.create(req.body);
+    await Qualification.create(req.body);
     return res.status(200).send('Qualification added');
   } catch (err) {
     return res.status(500).json(err);
@@ -111,7 +111,7 @@ app.get('/roles', async (req, res) => {
 
 app.post('/roles', async (req, res) => {
   try {
-    const role = await Role.create(req.body);
+    await Role.create(req.body);
     return res.status(200).send('Role added');
   } catch (err) {
     return res.status(500).json(err);
@@ -167,7 +167,7 @@ app.get('/duties/:callsign', async (req, res) => {
 // Add duty
 app.post('/duties', async (req, res) => {
   try {
-    const duty = await Duty.create(req.body);
+    await Duty.create(req.body);
     return res.status(200).send('Duty added');
   } catch (err) {
     return res.status(500).json(err);
@@ -198,7 +198,7 @@ app.put('/duties/:dutyId', async (req, res) => {
 // Create a blank model for a new month
 app.post('/schedules', async (req, res) => {
   try {
-    const schedule = await Schedule.create(req.body);
+    await Schedule.create(req.body);
     return res.status(200).send('Schedule created');
   } catch (err) {
     return res.status(500).json(err);
